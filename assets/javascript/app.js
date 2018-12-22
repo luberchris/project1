83e3f2ea0f94b497ce7e794acd7869955f2a38da
Jen Pirrone <pirronej104@gmail.com>
2018-12-22 10:57:37 -0500

    updating HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,37 @@
-var queryURL = "https://www.food2fork.com/api/get?key=f3f1b9a787e9efdbd4749be34123d9b5&rId=8c0314"
+var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=30ea9a46&_app_key=3d03668731b2112fff8aac21cb03c4c&q=onion+soup"
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
           
-          console.log(JSON.parse(response));
+          console.log(response);
 
-         var recipeResponse = JSON.parse(response);
-         console.log(recipeResponse.recipe.f2f_url);
+        //  var recipeResponse = JSON.parse(response);
+        //  console.log(recipeResponse.recipe.f2f_url);
 
-         $("#ingredhere").html(recipeResponse.recipe.f2f_url);
+        //  $("#ingredhere").html(recipeResponse.recipe.f2f_url);
 
      
 
   // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDMQKoXzxho2wVMf6eGMQRctvUAuo5I31A",
-        authDomain: "socialpantry-29e0d.firebaseapp.com",
-        databaseURL: "https://socialpantry-29e0d.firebaseio.com",
-        projectId: "socialpantry-29e0d",
-        storageBucket: "socialpantry-29e0d.appspot.com",
-        messagingSenderId: "329527149006"
-    };
-    firebase.initializeApp(config);
+    // var config = {
+    //     apiKey: "AIzaSyDMQKoXzxho2wVMf6eGMQRctvUAuo5I31A",
+    //     authDomain: "socialpantry-29e0d.firebaseapp.com",
+    //     databaseURL: "https://socialpantry-29e0d.firebaseio.com",
+    //     projectId: "socialpantry-29e0d",
+    //     storageBucket: "socialpantry-29e0d.appspot.com",
+    //     messagingSenderId: "329527149006"
+    // };
+    // firebase.initializeApp(config);
 
-    var database = firebase.database();
+    // var database = firebase.database();
 
-    var rUrl = recipeResponse.recipe.f2f_url;
+    // var rUrl = recipeResponse.recipe.f2f_url;
 
-    database.ref().push({
-        rUrl: rUrl,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+    // database.ref().push({
+    //     rUrl: rUrl,
+    //     dateAdded: firebase.database.ServerValue.TIMESTAMP
+    //   });
     
-    });
+    // });
